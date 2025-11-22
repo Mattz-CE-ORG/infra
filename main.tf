ccb88c28f8b11c3accfc07b80f313abffdd7d5fe
MAAAAAAAAATT <matthew@aliasintelligence.com>
2025-11-22 07:43:31 -0600

    fix: explicitly pass aws credentials to provider for robustness 🔐

--- main.tf
+++ main.tf
@@ -22,9 +22,10 @@ terraform {
 provider "aws" {
   region = var.aws_region
 
-  # Credentials from environment variables
-  # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-  # or from AWS credentials file/instance profile
+  # Credentials from environment variables (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+  # OR passed via variables below if set in Terraform Cloud as 'Terraform Variables'
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 # Module for the first Lightsail instance
